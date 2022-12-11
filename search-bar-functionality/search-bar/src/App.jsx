fd6d393492b11c387c95be47cdcace40fdfa1dc7
KritagyaPrasad <85838703+KritagyaPrasad@users.noreply.github.com>
2022-12-11 13:30:23 +0000

    functionality done

--- search-bar-functionality/search-bar/src/App.jsx
+++ search-bar-functionality/search-bar/src/App.jsx
@@ -5,6 +5,7 @@ import { Users } from "./users";
 function App() {
 
   const [query, setQuery] = useState("");
+  // console.log(Users.filter(user => user.first_name.toLowerCase().includes("fe")));
 
   function handleQuery(event) {
     setQuery(event.target.value);
@@ -12,10 +13,10 @@ function App() {
 
   return (
     <div className="app">
-      <input type="text" placeholder='search' className="search" onChange={handleQuery} />
+      <input type="text" placeholder='search...' className="search" onInput={handleQuery} />
       <ul className="list">
         {
-          Users.map((user) => (
+          Users.filter(user => user.first_name.toLowerCase().includes(query)).map((user) => (
             <li key={user.id} className="listItem">{user.first_name}</li>
           ))
         }
